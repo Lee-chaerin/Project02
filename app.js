a4297a45095d2cfca128f5e46aaed736cd11f6e9
Lee chaerin <5543705@naver.com>
2021-08-14 01:10:45 +0900

    chapter3

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-const age = parseInt(prompt("How old are you?"));
+const h1 = document.querySelector("div.hello:first-child h1");
 
-if (isNaN(age) || age < 0) {
-  console.log("Please write a real positive number.");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("You can drink.");
-} else if (age > 50 && age <= 80) {
-  console.log("You should exercise.");
-} else if (age === 100) {
-  console.log("wow you are wrise.");
-} else if (age > 80) {
-  console.log("You can do whatever you want.");
+function handleTitleClick() {
+  h1.classList.toggle("clicked");
 }
+
+h1.addEventListener("click", handleTitleClick);
